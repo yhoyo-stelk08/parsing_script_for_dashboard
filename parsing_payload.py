f17de3a2dcc35653827b76085fbd46498735b7d4
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-03-23 01:10:29 +0800

    feat: add new method for preparing raw data for 2G and 4G data.
     Changes to be committed:
    	modified:   parsing_payload.py

--- parsing_payload.py
+++ parsing_payload.py
@@ -9,12 +9,11 @@ from datetime import datetime, timedelta
 from add_func import export_to_csv, setCurDir, readConfigFile
 
 
-def setDfPayload2gUme(ume):
+def preparing_for_df_payload2g(ume):
     curdir = setCurDir()
     config_data = readConfigFile()
-
     dirdate = (datetime.today() - timedelta(hours=1,
-               minutes=25)).strftime('%Y-%m-%d')
+                                            minutes=25)).strftime('%Y-%m-%d')
     delta_hour = (datetime.today() - timedelta(hours=1, minutes=25))
     filedate = (delta_hour).strftime('%Y%m%d')
     last_quarter_minute = 15*(delta_hour.minute//15)
@@ -26,55 +25,137 @@ def setDfPayload2gUme(ume):
     extract_to_dir = curdir+os.sep+ume+os.sep + \
         'Trf_Thp_Paging_Avail_Pyld'+os.sep+'2G'
 
+    # change directory to filedir directory
+    os.chdir(filedir)
+
+    # list all files inside filedir directory
+    list_file = os.listdir(filedir)
+
+    # delete all files under dir band
+    for file in os.listdir(extract_to_dir):
+        # shutil.rmtree(file)
+        os.remove(extract_to_dir+os.sep+file)
+        # print(file)
+
+    # exracting raw files process
+    for file in list_file:
+        if ume == "UME_SUL":
+            zipFileName = config_data['SRC_UME']['Payload2G'][0]['prefix_fname_sul']
+        elif ume == "UME_PUMA":
+            zipFileName = config_data['SRC_UME']['Payload2G'][0]['prefix_fname_puma']
+        elif ume == "UME_KAL":
+            zipFileName = config_data['SRC_UME']['Payload2G'][0]['prefix_fname_kal']
+        else:
+            print("No files in listed files.")
+
+        # print(filedatetime)
+        pattern = zipFileName+'_'+filedatetime
+        # print(pattern)
+        result = re.search(pattern+'.+', file)
+        if result:
+            print(result.group(0))
+            filename = result.group(0)
+            # extract files from filedir into extract_to_dir
+            print('Extracting file '+filename)
+            with zipfile.ZipFile(filedir+os.sep+filename, 'r') as zip_ref:
+                zip_ref.extractall(extract_to_dir+os.sep)
+                print("File "+filename+" telah di extract ke "+extract_to_dir)
+
+    # delete unneeded files
+    for file in os.listdir(extract_to_dir):
+        result = re.search('.+kpis.+', file)
+        if result:
+            # print(result.group(0))
+            # delete file kpis in extract_to_dir
+            kpis_files = result.group(0)
+            os.remove(extract_to_dir+os.sep+kpis_files)
+
+
+def preparing_for_df_payload4g(ume, band):
+    curdir = setCurDir()
+    config_data = readConfigFile()
+
+    dirdate = (datetime.today() - timedelta(hours=1,
+               minutes=25)).strftime('%Y-%m-%d')
+    delta_hour = (datetime.today() - timedelta(hours=1, minutes=25))
+    filedate = (delta_hour).strftime('%Y%m%d')
+    last_quarter_minute = 15*(delta_hour.minute//15)
+    qtime = delta_hour.replace(minute=last_quarter_minute).strftime('%H%M')
+    filedatetime = filedate + qtime
+
+    filedir = config_data['SRC_UME']['Payload4G'][0]['src_dir'] + \
+        os.sep + dirdate
+    extract_to_dir = curdir+os.sep+ume+os.sep + \
+        'Trf_Thp_Paging_Avail_Pyld'+os.sep+'4G'+os.sep+band
+
+    # change directory to filedir directory
+    os.chdir(filedir)
+
+    # list all files inside filedir directory
+    list_file = os.listdir(filedir)
+
+    # delete all files under dir band
+    for file in os.listdir(extract_to_dir):
+        # shutil.rmtree(file)
+        os.remove(extract_to_dir+os.sep+file)
+        # print(file)
+
+    # extract files from filedir
+    for file in list_file:
+        if band == "FDD":
+            if ume == "UME_SUL":
+                zipFileName = config_data['SRC_UME']['Payload4G'][0]['prefix_fname_fdd_sul']
+            elif ume == "UME_PUMA":
+                zipFileName = config_data['SRC_UME']['Payload4G'][0]['prefix_fname_fdd_puma']
+            elif ume == "UME_KAL":
+                zipFileName = config_data['SRC_UME']['Payload4G'][0]['prefix_fname_fdd_kal']
+        else:
+            if ume == "UME_SUL":
+                zipFileName = config_data['SRC_UME']['Payload4G'][0]['prefix_fname_tdd_sul']
+            elif ume == "UME_PUMA":
+                zipFileName = config_data['SRC_UME']['Payload4G'][0]['prefix_fname_tdd_puma']
+            elif ume == "UME_KAL":
+                zipFileName = config_data['SRC_UME']['Payload4G'][0]['prefix_fname_tdd_kal']
+        # print(filedatetime)
+        pattern = zipFileName+'_'+filedatetime
+        # print(pattern)
+        result = re.search(pattern+'.+', file)
+        if result:
+            # print(result.group(0))
+            filename = result.group(0)
+            print('Extracting file '+filename)
+            with zipfile.ZipFile(filedir+os.sep+filename, 'r') as zip_ref:
+                zip_ref.extractall(extract_to_dir+os.sep)
+                print("File "+filename+" telah di extract ke "+extract_to_dir)
+
+    # delete unneeded files
+    for file in os.listdir(extract_to_dir):
+        result = re.search('.+kpis.+', file)
+        if result:
+            # print(result.group(0))
+            # delete file kpis in extract_to_dir
+            kpis_files = result.group(0)
+            os.remove(extract_to_dir+os.sep+kpis_files)
+
+
+def setDfPayload2gUme(ume):
+    curdir = setCurDir()
+
+    # setting path for file to extract
+    data_dir = curdir+os.sep+ume+os.sep + \
+        'Trf_Thp_Paging_Avail_Pyld'+os.sep+'2G'
+
     df_result = pd.DataFrame()
 
     if ume == "UME_SUL" or ume == "UME_KAL" or ume == "UME_PUMA":
 
-		# change directory to filedir directory
-        os.chdir(filedir)
-
-        # list all files inside filedir directory
-        list_file = os.listdir(filedir)
-
-        # delete all files under dir band
-        for file in os.listdir(extract_to_dir):
-            # shutil.rmtree(file)
-            os.remove(extract_to_dir+os.sep+file)
-            # print(file)
-
-        # extract files from filedir
-        for file in list_file:
-            if ume == "UME_SUL":
-                zipFileName = config_data['SRC_UME']['Payload2G'][0]['prefix_fname_sul']
-            elif ume == "UME_PUMA":
-                zipFileName = config_data['SRC_UME']['Payload2G'][0]['prefix_fname_puma']
-            else:
-                zipFileName = config_data['SRC_UME']['Payload2G'][0]['prefix_fname_kal']
-            # print(filedatetime)
-            pattern = zipFileName+'_'+filedatetime
-            # print(pattern)
-            result = re.search(pattern+'.+', file)
-            if result:
-                print(result.group(0))
-                filename = result.group(0)
-                print('Extracting file '+filename)
-                with zipfile.ZipFile(filedir+os.sep+filename, 'r') as zip_ref:
-                    zip_ref.extractall(extract_to_dir+os.sep)
-                    print("File "+filename+" telah di extract ke "+extract_to_dir)
-
-        # delete unneeded files
-        for file in os.listdir(extract_to_dir):
-            result = re.search('.+kpis.+', file)
-            if result:
-                # print(result.group(0))
-                # delete file kpis in extract_to_dir
-                kpis_files = result.group(0)
-                os.remove(extract_to_dir+os.sep+kpis_files)
+        # method for preparing raw data
+        # preparing_for_df_payload2g(ume)
 
         # set dataframe process
-        for file in os.listdir(extract_to_dir):
+        for file in os.listdir(data_dir):
             # print(file)
-            df_res = pd.read_csv(extract_to_dir+os.sep+file, thousands=',')
+            df_res = pd.read_csv(data_dir+os.sep+file, thousands=',')
             df_res['GRANULARITY'] = 900
             df_res['tech'] = '2G'
             df_res['OSS'] = ume
@@ -88,7 +169,7 @@ def setDfPayload2gUme(ume):
             df_res['COLLECTTIME'] = df_res['TANGGAL'].astype(
                 str) + df_res['JAM'].astype(str)
             # print(df_res['COLLECTTIME'])
-            if ume == "UME_SUL" or ume == "UME_PUMA" :
+            if ume == "UME_SUL" or ume == "UME_PUMA":
                 df_result = df_res[
                     [
                         'COLLECTTIME', 'GRANULARITY', 'SubnetWork ID', 'SITE ID', 'BTS ID', 'OSS', 'tech', 'TOTAL PAYLOAD (EDGE+GPRS)'
@@ -116,78 +197,21 @@ def setDfPayload2gUme(ume):
 
 def setDfPayload4gUme(ume, band):
     curdir = setCurDir()
-    config_data = readConfigFile()
 
-    dirdate = (datetime.today() - timedelta(hours=1,
-               minutes=25)).strftime('%Y-%m-%d')
-    delta_hour = (datetime.today() - timedelta(hours=1, minutes=25))
-    filedate = (delta_hour).strftime('%Y%m%d')
-    last_quarter_minute = 15*(delta_hour.minute//15)
-    qtime = delta_hour.replace(minute=last_quarter_minute).strftime('%H%M')
-    filedatetime = filedate + qtime
-
-    filedir = config_data['SRC_UME']['Payload4G'][0]['src_dir'] + \
-        os.sep + dirdate
-    extract_to_dir = curdir+os.sep+ume+os.sep + \
+    data_dir = curdir+os.sep+ume+os.sep + \
         'Trf_Thp_Paging_Avail_Pyld'+os.sep+'4G'+os.sep+band
 
     df_result = pd.DataFrame()
 
     if ume == "UME_SUL" or ume == "UME_KAL" or ume == "UME_PUMA":
 
-		# change directory to filedir directory
-        os.chdir(filedir)
+        # method for preparing raw data
+        # preparing_for_df_payload4g(ume, band)
 
-		# list all files inside filedir directory
-        list_file = os.listdir(filedir)
-
-        # delete all files under dir band
-        for file in os.listdir(extract_to_dir):
-            # shutil.rmtree(file)
-            os.remove(extract_to_dir+os.sep+file)
+        # set dataframe process
+        for file in os.listdir(data_dir):
             # print(file)
-
-        # extract files from filedir
-        for file in list_file:
-            if band == "FDD":
-                if ume == "UME_SUL":
-                    zipFileName = config_data['SRC_UME']['Payload4G'][0]['prefix_fname_fdd_sul']
-                elif ume == "UME_PUMA":
-                    zipFileName = config_data['SRC_UME']['Payload4G'][0]['prefix_fname_fdd_puma']
-                elif ume == "UME_KAL":
-                    zipFileName = config_data['SRC_UME']['Payload4G'][0]['prefix_fname_fdd_kal']
-            else:
-                if ume == "UME_SUL":
-                    zipFileName = config_data['SRC_UME']['Payload4G'][0]['prefix_fname_tdd_sul']
-                elif ume == "UME_PUMA":
-                    zipFileName = config_data['SRC_UME']['Payload4G'][0]['prefix_fname_tdd_puma']
-                elif ume == "UME_KAL":
-                    zipFileName = config_data['SRC_UME']['Payload4G'][0]['prefix_fname_tdd_kal']
-            # print(filedatetime)
-            pattern = zipFileName+'_'+filedatetime
-            # print(pattern)
-            result = re.search(pattern+'.+', file)
-            if result:
-                # print(result.group(0))
-                filename = result.group(0)
-                print('Extracting file '+filename)
-                with zipfile.ZipFile(filedir+os.sep+filename, 'r') as zip_ref:
-                    zip_ref.extractall(extract_to_dir+os.sep)
-                    print("File "+filename+" telah di extract ke "+extract_to_dir)
-
-        # delete unneeded files
-        for file in os.listdir(extract_to_dir):
-            result = re.search('.+kpis.+', file)
-            if result:
-                # print(result.group(0))
-                # delete file kpis in extract_to_dir
-                kpis_files = result.group(0)
-                os.remove(extract_to_dir+os.sep+kpis_files)
-
-		# set dataframe process
-        for file in os.listdir(extract_to_dir):
-            # print(file)
-            df_res = pd.read_csv(extract_to_dir+os.sep+file, thousands=",")
+            df_res = pd.read_csv(data_dir+os.sep+file, thousands=",")
             df_res['GRANULARITY'] = 900
             df_res['tech'] = '4G ' + band
             df_res['OSS'] = ume
@@ -325,5 +349,5 @@ def counting_payload(item):
     return df_result
 
 
-df = setDfPayload4gUme("UME_SUL","TDD")
-print(df)
+df = parsing_payload()
+print(df.dtypes)
